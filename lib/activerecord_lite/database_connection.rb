05a37e118ed369ea273d172efb2136e390ac03f3
Osita Cyril Iyadi <Iyadicy@gmail.com>
2025-06-21 17:49:48 -0400

    Implement DatabaseConnection class for in-memory SQLite database

---
+++ lib/activerecord_lite/database_connection.rb
@@ -0,0 +1,17 @@
+require 'sqlite3'
+require 'singleton'
+
+module ActiveRecordLite
+  class DatabaseConnection
+    include singleton
+
+    def initialize
+      @db = SQlITE3::Database.new ":memory:"
+      @db.results_as_hash = true
+    end
+
+    def execute(*args)
+      @db.execute(*args)
+    end
+  end
+end
