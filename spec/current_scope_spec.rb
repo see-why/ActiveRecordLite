2d1708e6541f2696805c5261a298218758c90d22
Osita Cyril Iyadi <Iyadicy@gmail.com>
2025-07-01 23:52:04 -0400

    Add RSpec tests for ActiveRecordLite::CurrentScope to verify scope attribute behavior and default initialization

--- spec/current_scope_spec.rb
+++ spec/current_scope_spec.rb
@@ -1,8 +1,16 @@
 require 'spec_helper'
-require 'activerecord_lite/current_scope'
+require 'activerecord_lite'
 
 describe ActiveRecordLite::CurrentScope do
-  it 'loads the module' do
-    expect(defined?(ActiveRecordLite::CurrentScope)).to eq('constant')
+  subject(:current_scope) { described_class.new }
+
+  it 'can set and get the scope attribute' do
+    fake_scope = double('Scope')
+    current_scope.scope = fake_scope
+    expect(current_scope.scope).to eq(fake_scope)
+  end
+
+  it 'initializes with nil scope by default' do
+    expect(current_scope.scope).to be_nil
   end
 end
