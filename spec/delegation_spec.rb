47bc818f4a24d794ef2510a0878910a02f568911
Osita Cyril Iyadi <Iyadicy@gmail.com>
2025-07-01 23:52:12 -0400

    Add RSpec tests for ActiveRecordLite::Delegation to verify DELEGATED_CLASSES array structure and immutability

--- spec/delegation_spec.rb
+++ spec/delegation_spec.rb
@@ -6,4 +6,15 @@ describe ActiveRecordLite::Delegation do
   it 'loads the module' do
     expect(defined?(ActiveRecordLite::Delegation)).to eq('constant')
   end
+
+  describe '::DELEGATED_CLASSES' do
+    it 'is an array of classes/modules to delegate to' do
+      expect(described_class::DELEGATED_CLASSES).to be_an(Array)
+      expect(described_class::DELEGATED_CLASSES).to include(ActiveRecordLite::Relation)
+    end
+
+    it 'is frozen' do
+      expect(described_class::DELEGATED_CLASSES).to be_frozen
+    end
+  end
 end
